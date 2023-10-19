3fffea3250c06025a88335dca70073d8fbbbfccb
ValenciaLim <101036460+ValenciaLim@users.noreply.github.com>
2023-10-20 01:56:23 +0800

    Add files via upload

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Navbar.scss';
 import { images } from '../../constants';
-import Scrollspy from 'react-scrollspy';
 
 const Navbar = () => {
   return (
@@ -9,15 +8,13 @@ const Navbar = () => {
       <div className="app__navbar-logo">
         <img src={images.logo} alt="logo"></img>
       </div>
-      <div className="app__navbar-links">
-        <Scrollspy items={['home', 'skills', 'experiences', 'contact']} currentClassName="is-current">
-        {['home', 'skills', 'experiences', 'contact'].map((item) => (
-            <li className="app__flex p-text" key="{'link-${item}'}">
-              < a href={`#${item}`}>{item}</a>
-            </li>
-          ))}
-        </Scrollspy>
-      </div>
+      <ul className="app__navbar-links">
+      {['home', 'skills', 'experience', 'portfolio', 'contact'].map((item) => (
+          <li className="app__flex p-text" key="{'link-${item}'}">
+            < a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
