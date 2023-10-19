6d37d5093391ff2658223c6f0244d3f457ed4898
ValenciaLim <101036460+ValenciaLim@users.noreply.github.com>
2023-10-20 02:01:30 +0800

    Delete src/components/container/index.js

--- src/components/container/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from './Header/Header';
-export { default as Home } from './Home/Home';
-export { default as Footer } from './Footer/Footer';
-export { default as Experience } from './Experience/Experience';
-export { default as Portfolio } from './Portfolio/Portfolio';
-export { default as Skills } from './Skills/Skills';
