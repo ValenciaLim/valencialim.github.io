1e36aeb3daf36620ab6964830b9331d79229a2a0
Valencia <valencialimrt@gmail.com>
2023-10-20 02:38:13 +0800

    Modify Navbar for Mobile View

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import './Header.scss';
 import { HiMenu } from 'react-icons/hi';
-import { AiOutlineHome, AiOutlineFolderOpen } from 'react-icons/ai';
-import { BsPerson, BsSend } from 'react-icons/bs'
+import { AiOutlineHome } from 'react-icons/ai';
+import { BsSend } from 'react-icons/bs'
+import { IoIosCodeWorking } from 'react-icons/io';
+import { MdWorkOutline } from 'react-icons/md';
 import { SiSkillshare } from 'react-icons/si'
 import { Navbar, NavigationDots } from '../../components';
 
@@ -20,13 +22,13 @@ const Header = () => {
                   < a href="#home"><AiOutlineHome /></a>
                 </li>
                 <li>
-                  < a href="#about"><BsPerson /></a>
-                </li>
-                <li>
                   < a href="#skills"><SiSkillshare /></a>
                 </li>
                 <li>
-                  < a href="#projects"><AiOutlineFolderOpen /></a>
+                  < a href="#experience"><MdWorkOutline /></a>
+                </li>
+                <li>
+                  < a href="#portfolio"><IoIosCodeWorking /></a>
                 </li>
                 <li>
                   < a href="#contact"><BsSend /></a>
